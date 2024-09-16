2069c33c1c1913e4b3a237c35539a1b0c792f7a3
andreijsecor <andreijsecor@gmail.com>
2024-09-16 14:07:30 -0700

    reformatting

--- server/static/javascript/output.js
+++ server/static/javascript/output.js
@@ -6,17 +6,16 @@ function startCheckThread() {
 
 function checkSummary() {
     currUrl = window.location.href;
-    response = fetch(currUrl.substring(0,currUrl.length - 3) + "/verify");
-    response.then((response) => {
-      if (response.status != 200) {
-        clearInterval(checkSummaryIntervalId);
-        load = document.getElementById("loading");
-        parent = load.parentElement;
-        frame = document.createElement("iframe");
-        frame.setAttribute("src", "/out");
-        frame.style.setProperty('width', "100%");
-        frame.style.setProperty("aspect-ratio", "3 / 2");
-        parent.replaceChild(frame, load);
-      }
+    fetch(currUrl + "/verify").then((response) => {
+        if (response.status != 200) {
+            clearInterval(checkSummaryIntervalId);
+            load = document.getElementById("loading");
+            parent = load.parentElement;
+            frame = document.createElement("iframe");
+            frame.setAttribute("src", "/out");
+            frame.style.setProperty('width', "100%");
+            frame.style.setProperty("aspect-ratio", "3 / 2");
+            parent.replaceChild(frame, load);
+        }
     });
-  }
+}
