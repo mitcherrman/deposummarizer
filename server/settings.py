7c59602f5c4d3eb002a510aed1aef860889af7ca
andreijsecor <andreijsecor@gmail.com>
2024-09-25 13:19:53 -0700

    added 404.html

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0%31bd)ih*a7bzlbmrf3m!lyxrp)9se!%*n(%(#9v!zg#ggk7t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1'] #change for production
 
 # Application definition
 
