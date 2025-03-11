4eb2364da9093701d76ee05715f7a11dd8fdb77b
andreijsecor <andreijsecor@gmail.com>
2025-03-11 14:12:09 -0700

    bug fix

--- server/settings.py
+++ server/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-import util
+from . import util
 import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
