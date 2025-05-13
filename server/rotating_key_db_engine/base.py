ee1d481b9b7d1b04e510ad6154c4bad0a2c4e82d
andreijsecor <andreijsecor@gmail.com>
2025-05-13 14:51:05 -0700

    better caching of rotating db keys

--- server/rotating_key_db_engine/base.py
+++ server/rotating_key_db_engine/base.py
@@ -2,17 +2,34 @@ from django.db.backends.postgresql import base
 from server import util
 from decouple import config
 from django.conf import settings
+from psycopg import OperationalError
 import json
 
 class DatabaseWrapper(base.DatabaseWrapper):
 
+    cached_auth = {
+        "user": "",
+        "password": ""
+    }
+
     def get_connection_params(self):
         conn_params = super().get_connection_params()
         if not (settings.DEBUG | settings.TEST_WITH_LOCAL_DB):
-            try:
-                secret = json.loads(util.get_secret(config("DB_SECRET_ARN")))
-                conn_params['user'] = secret['username']
-                conn_params['password'] = secret['password']
-            except Exception as e:
-                raise Exception("Failed to retrieve most recent connection details", e)
+            conn_params['user'] = self.cached_auth['username']
+            conn_params['password'] = self.cached_auth['password']
         return conn_params
+    
+    def refresh_conn_cache(self):
+        secret = json.loads(util.get_secret(config("DB_SECRET_ARN")))
+        self.cached_auth['user'] = secret['username']
+        self.cached_auth['password'] = secret['password']
+    
+    def get_new_connection(self, conn_params):
+        try:
+            return super().get_new_connection(conn_params)
+        except OperationalError as e:
+            if e.pgcode == '28P01':
+                self.refresh_conn_cache()
+                return super().get_new_connection(conn_params)
+            else:
+                raise e
