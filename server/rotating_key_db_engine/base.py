4867ea0ee1050b7016a5289ba2a8f280a11a4d94
andreijsecor <andreijsecor@gmail.com>
2025-05-14 14:51:34 -0700

    bug fix

--- server/rotating_key_db_engine/base.py
+++ server/rotating_key_db_engine/base.py
@@ -14,19 +14,24 @@ class DatabaseWrapper(base.DatabaseWrapper):
 
     def get_connection_params(self):
         conn_params = super().get_connection_params()
-        if not (settings.DEBUG | settings.TEST_WITH_LOCAL_DB):
+        if not (settings.DEBUG or settings.TEST_WITH_LOCAL_DB):
             conn_params['user'] = self.cached_auth['username']
             conn_params['password'] = self.cached_auth['password']
         return conn_params
     
-    def refresh_conn_cache(self):
+    def refresh_conn_cache(self, conn_params=None):
         secret = json.loads(util.get_secret(config("DB_SECRET_ARN")))
         self.cached_auth['username'] = secret['username']
         self.cached_auth['password'] = secret['password']
+        if conn_params:
+            conn_params['user'] = secret['username']
+            conn_params['password'] = secret['password']
     
     def get_new_connection(self, conn_params):
         try:
             return super().get_new_connection(conn_params)
         except OperationalError as e:
-            self.refresh_conn_cache()
+            if settings.DEBUG or settings.TEST_WITH_LOCAL_DB:
+                raise e
+            self.refresh_conn_cache(conn_params)
             return super().get_new_connection(conn_params)
